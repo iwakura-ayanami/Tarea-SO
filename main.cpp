01752b60f82b867eb77e1da46f7aaa8686347a56
BenjaminOyanedel <benjamin.oyanedelb@utem.cl>
2024-07-22 17:26:00 -0400

    Se agregan comentarios de las funciones y se restringe el número de procesos de entrada a no más de seis.

--- main.cpp
+++ main.cpp
@@ -8,26 +8,25 @@
 
 struct Proceso {
     std::string nombre;
-    double tamano;
+    double      tamano;
     std::string unidad;
-    int numPaginas;
-    int marcosAsignados;
-    float tiempoAcceso;
-    float tiempoTransferencia;
-    float tiempoEjecucion;
-    float tiempoFallas;
-    float turnaroundTime;
+    int         numPaginas;
+    int         marcosAsignados;
+    float       tiempoAcceso;
+    float       tiempoTransferencia;
+    float       tiempoEjecucion;
+    float       tiempoFallas;
+    float       turnaroundTime;
 };
 
 
-// const int RAM_SIZE = 2 * 1024 * 1024; // 2MB en bytes
-const int RAM_SIZE = 20 * 1024 * 1024; // 20MB en bytes
-//const int BLOCK_SIZE = 512; // bytes
-const int BLOCK_SIZE = 4096; // bytes
+const int RAM_SIZE = 20 * 1024 * 1024; // 2MB
+const int BLOCK_SIZE = 4096; // 512B
 const int MAX_PROCESOS = 6;
 
-std::vector<Proceso> procesos;
+std::vector<Proceso> procesos; //Vector de procesos a analizar.
 
+//Convierte cualquier unidad de entrada a bytes
 double convertirABytes(double tamano, std::string unidad) {
     if (unidad == "b") return tamano / 8.0;
     if (unidad == "B") return tamano;
@@ -37,29 +36,32 @@ double convertirABytes(double tamano, std::string unidad) {
     return 0;
 }
 
+//Calcula el número de paginas para el tamaño de un proceso y su tamaño de bloque
 int calcularNumeroPaginas(double tamanoBytes, int tamanoBloque) {
     return std::ceil(tamanoBytes / tamanoBloque);
 }
 
+//Esta función determina el número de marcos asignados a cada proceso en 
+//relacion a su proporcion de páginas respecto a las páginas totales y los marcos disponibles.
 void distribucionProporcional() {
     float totalPaginas = 0;
     for (auto& p : procesos) {
-        totalPaginas += p.numPaginas;
+        totalPaginas += p.numPaginas;//Suma todos los números de páginas y los guarda.
     }
     
     int marcosDisponibles = RAM_SIZE / BLOCK_SIZE;
     
     for (auto& p : procesos) {
         double proporcion = (double)p.numPaginas / totalPaginas;
-        if(proporcion * marcosDisponibles < 1.0)
-            p.marcosAsignados = 1;
+        if(proporcion * marcosDisponibles < 1.0)        //Según si la proporcion es menor a los marcos disponibles
+            p.marcosAsignados = 1;                      //Asignamos 1 en caso de estar entre 0 y 1.
         else
             p.marcosAsignados = std::floor(proporcion * marcosDisponibles);
     }
 }
 
 
-
+//Muestra páginas y marcos.
 void mostrarPaginasYMarcos() {
     int totalPaginas = 0;
     int totalMarcos = RAM_SIZE / BLOCK_SIZE;
@@ -93,21 +95,23 @@ void mostrarPaginasYMarcos() {
     std::cout << "Fragmentación total: " << std::fixed << std::setprecision(2) << fragmentacionTotal << " bytes" << std::endl;
 }
 
-
+//Toma un archivo .csv y extrae los datos de una forma específica como se puede ver en el archivo procesos.csv
 void ingresar_desde_archivo(){
     std::ifstream read;
     read.open("procesos.csv", std::ios::in);
     std::string line, word;
     Proceso p;
-    getline(read, line);
+    getline(read, line);                //Ignora la primera línea
     std::vector<std::string> fila;
-     while(read.is_open()){
+    int i = 0;
+     while(read.is_open() && i < MAX_PROCESOS){
+        i++;
         fila.clear();
         getline(read, line);
         std::stringstream s(line);
         if(line == "")break;
         while (getline(s, word, ',')) {
-            fila.push_back(word);
+            fila.push_back(word);           //Obtiene los campos
         }
         fila.push_back(word);
 
@@ -116,7 +120,7 @@ void ingresar_desde_archivo(){
         double tamano = std::stod(fila[1]);
         p.tamano = convertirABytes(tamano, p.unidad);
         p.tiempoAcceso = std::stof(fila[3]);
-        p.tiempoEjecucion = std::stof(fila[5]);
+        p.tiempoEjecucion = std::stof(fila[5]);             //Asigna los campos a sus variables correspondientes
         p.tiempoTransferencia = std::stof(fila[4]);
         p.numPaginas = calcularNumeroPaginas(p.tamano, BLOCK_SIZE);
         
@@ -125,12 +129,17 @@ void ingresar_desde_archivo(){
     read.close();
 }
 
+//Calcula las veces que se cargan y descargan las páginas de un proceso hasta 
+//que solo queden pendientes las páginas sobrantes del mismo y así mismo calcula el número de stas últimas.
+//Luego, guarda los resultados en las variables "vecesCompletas" y "sobrantes" respectivamente.
 void CalcularCargasDescargas(int pags, int marcos, int &vecesCompletas, int &sobrantes){
   int aux = pags/marcos;
   aux += (aux-1);
   vecesCompletas = aux;
   sobrantes = pags % marcos;
 }
+
+//Calcula el tiempo de falla y el turnAroundTime y los guarda en el struct de cada proceso. 
 void calcularTiempos() {
     for (auto& p : procesos) {
         if(p.numPaginas < p.marcosAsignados)
@@ -151,30 +160,31 @@ void calcularTiempos() {
     }
 }
 
+//Muestra los resultados obtenidos por consola el tiempo de fallas, turn around time, paginas y marcos.
 void mostrarResultados() {
     std::cout << "\nResultados:" << std::endl;
     std::cout << std::left << std::setw(10) << "Proceso" 
               << std::setw(20) << "Tiempo de Fallas (ms)" 
               << std::setw(20) << "Turn around Time(ms)"
-              << std::setw(20) << "paginas " 
+              << std::setw(20) << "paginas "                //Imprime el header
               << std::setw(20) << "marcos" << std::endl;
     std::cout << std::string(50, '-') << std::endl;
 
     for (const auto& p : procesos) {
         std::cout << std::left << std::setw(10) << p.nombre 
-                  << std::setw(20) << p.tiempoFallas 
+                  << std::setw(20) << p.tiempoFallas        //Imprime cada uno de los procesos
                   << std::setw(20) << p.turnaroundTime
                   << std::setw(20) <<p.numPaginas 
                   << std::setw(20) <<p.marcosAsignados << std::endl;
     }
 }
 
-
+//Función principal
 int main() {
-    ingresar_desde_archivo();
-    distribucionProporcional();
-    calcularTiempos();
-    mostrarResultados();
+    ingresar_desde_archivo();   //Ingreso el archivo y guardo los procesos
+    distribucionProporcional(); //Calculo los marcos y páginas asignados de cada proceso
+    calcularTiempos();          //Calculo el tiempo de fallas y el turn around time de cada proceso
+    mostrarResultados();        //Muestro estas últimas 4 variables recién calculadas
     system("PAUSE");
     return 0;
 }
